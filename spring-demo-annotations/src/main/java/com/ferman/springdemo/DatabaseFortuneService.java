81fbbdebed0eeeed955003ac33e44a8b2a77217c
Ferman Naqvi <naqviferman@gmail.com>
2020-03-04 00:21:10 +0530

    Qualifiers for DI

---
+++ spring-demo-annotations/src/main/java/com/ferman/springdemo/DatabaseFortuneService.java
@@ -0,0 +1,12 @@
+package com.ferman.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
---
+++ spring-demo-annotations/src/main/java/com/ferman/springdemo/RESTFortuneService.java
@@ -0,0 +1,12 @@
+package com.ferman.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RESTFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}

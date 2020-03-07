c2b364e91bc2590a59f1c3c3c174a35334f19691
Ferman Naqvi <naqviferman@gmail.com>
2020-03-08 00:45:25 +0530

    Post Construct usage

--- spring-demo-annotations/src/main/java/com/ferman/springdemo/PropertyFileFortuneService.java
+++ spring-demo-annotations/src/main/java/com/ferman/springdemo/PropertyFileFortuneService.java
@@ -3,6 +3,7 @@ package com.ferman.springdemo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Random;
 
 @Component
@@ -15,14 +16,23 @@ public class PropertyFileFortuneService implements FortuneService {
     @Value("${fortune_3}")
     private String fortune_2;
 
+    private String[] fortunes;
+
     public PropertyFileFortuneService() {
         System.out.println("PropertyFileFortuneService: Default Constructor");
-        System.out.println("fortune_1 = " + fortune_1);     // Would be null
+        System.out.println("fortune_1 = " + fortune_1);     // Would be null as @Value has not run till this point
+    }
+
+    @PostConstruct
+    public void dataSetup() {
+        fortunes = new String[3];
+        fortunes[0] = fortune_1;
+        fortunes[1] = fortune_2;
+        fortunes[2] = fortune_3;
     }
 
     @Override
     public String getFortune() {
-        String[] fortunes = {fortune_1, fortune_2, fortune_3};
         Random random = new Random();
         return fortunes[random.nextInt(fortunes.length)];
     }

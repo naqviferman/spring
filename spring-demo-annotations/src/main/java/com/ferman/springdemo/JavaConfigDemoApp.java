9251283ed771d8f780840602fe840eb276a5e95d
Ferman Naqvi <naqviferman@gmail.com>
2020-03-09 12:47:19 +0530

    Annotation Based Context- Java Config (no xml)

---
+++ spring-demo-annotations/src/main/java/com/ferman/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,24 @@
+package com.ferman.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JavaConfigDemoApp {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+        Coach thatSillyCoach = context.getBean("tennisCoach", TennisCoach.class);
+
+        System.out.println(thatSillyCoach.getDailyWorkout());
+        System.out.println(thatSillyCoach.getDailyFortune());
+
+//        SwimCoach mySwimCoach = context.getBean(SwimCoach.class);
+//        System.out.println(mySwimCoach.getEmail());
+//        System.out.println(mySwimCoach.getTeam());
+
+//        System.out.println(mySwimCoach.getDailyFortune());
+
+        context.close();
+    }
+}
---
+++ spring-demo-annotations/src/main/java/com/ferman/springdemo/SportConfig.java
@@ -0,0 +1,9 @@
+package com.ferman.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ferman.springdemo")
+public class SportConfig {
+}
